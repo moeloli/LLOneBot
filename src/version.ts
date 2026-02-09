36a64ce1906af7d98592ad92f62bb04bae610e90
idranme <96647698+idranme@users.noreply.github.com>
2026-02-09 23:11:23 +0800

    chore(release): bump version to 7.8.2

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import packageJson from '../package-dist.json'
 
-export const version = '7.8.1'
+export const version = '7.8.2'
 
 export const writeVersion = ()=>{
   const pkgJsonPath = './package-dist.json'
